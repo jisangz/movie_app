1c565926df43b1076792d7e87f3518af4d1269b3
정승환 <shinjisang@gridwiz.com>
2021-12-20 00:14:51 +0900

    Add netlify functions

---
+++ functions/hello.js
@@ -0,0 +1,11 @@
+exports.hendler = async function(event, context){
+  return{
+    statusCode : 200,
+    body: JSON.stringify({
+      name:'HEROPY',
+      age: 85,
+      email:'thesecon@gmail.com'
+    })
+  }
+
+}
